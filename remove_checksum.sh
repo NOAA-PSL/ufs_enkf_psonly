ec288a51c03f20133bfefefb992b9a86de01f042
jswhit2 <Jeffrey.S.Whitaker@noaa.gov>
2025-02-17 19:58:46 +0000

    update

--- remove_checksum.sh
+++ remove_checksum.sh
@@ -1,6 +1,6 @@
 nanal=1
-while [ $nanal -le 80 ]; do
+while [ $nanal -le $2 ]; do
 charnanal="mem"`printf %03i $nanal`
-/work/noaa/gsienkf/whitaker/miniconda3/bin/python remove_checksum.py   /work2/noaa/gsienkf/jwhitake/C96ufs_psonly1/2020010100/${charnanal}/INPUT
+python remove_checksum.py   ${1}/${charnanal}/INPUT
 nanal=$((nanal+1))
 done
